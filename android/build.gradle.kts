df6bb7635b39d936c553fc98de26b336abc108df
Biztonic Admin <admin@biztonic.com>
2026-07-05 11:44:10 +0530

    fix(android): resolve legacy plugin namespace, NDK, compileSdk 34, and minSdk 23 for release APK build

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -13,7 +13,32 @@ subprojects {
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
 subprojects {
-    project.evaluationDependsOn(":app")
+    val configureProject = {
+        if (project.hasProperty("android")) {
+            val android = project.extensions.findByName("android")
+            if (android is com.android.build.gradle.BaseExtension) {
+                android.compileSdkVersion(34)
+            }
+        }
+        if (plugins.hasPlugin("com.android.library")) {
+            val android = extensions.getByType(com.android.build.gradle.LibraryExtension::class.java)
+            if (android.namespace == null) {
+                if (project.name == "flutter_bluetooth_serial") {
+                    android.namespace = "io.github.edufolly.flutterbluetoothserial"
+                } else {
+                    android.namespace = "com.example." + project.name.replace("-", "_")
+                }
+            }
+        }
+    }
+
+    if (project.state.executed) {
+        configureProject()
+    } else {
+        project.afterEvaluate {
+            configureProject()
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
